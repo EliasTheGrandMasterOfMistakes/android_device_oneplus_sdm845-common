4daa56f6d4f6a6084175016aee295713a515c85d
mathieuchartier <mathieuchartier@users.noreply.github.com>
2024-11-22 14:44:50 -0300

    sdm845-common: set PRODUCT_SYSTEM_SERVER_COMPILER_FILTER Speed profile services and wifi-service to reduce RAM and storage.

--- common.mk
+++ common.mk
@@ -455,6 +455,9 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc \
     vendor/ih8sn
 
+# Speed profile services and wifi-service to reduce RAM and storage.
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
